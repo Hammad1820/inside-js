73a206740789b29b11ed16cc675b480ecab3f0ff
Hammad1820 <48063834+Hammad1820@users.noreply.github.com>
2024-03-17 13:21:08 +0100

    add

--- lesson-plans/for-loops/2-integrate/multiplication/script.js
+++ lesson-plans/for-loops/2-integrate/multiplication/script.js
@@ -1,10 +1,14 @@
 import { readNumber, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('do-math').addEventListener('click', () => {
   debugger;
 
   // read user values
+  let left=readNumber('left');
+  let right=readNumber('right');
 
+  let value=left*right;
+  let displays=display('product',value)
   // use a for loop to multiply the two numbers
 
   // display the product
--- lesson-plans/for-loops/2-integrate/repeat-characters/script.js
+++ lesson-plans/for-loops/2-integrate/repeat-characters/script.js
@@ -1,15 +1,20 @@
 import { readNumber, readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('repeat-them').addEventListener('click', () => {
   debugger;
 
   // read user values
+  let character=readString('user-text');
+  let repeat = readNumber('number-of-times')
+  let after = '';
 
   // repeat the characters in the text
 
-  for (_; _; _) {
-    for (_; _; _) {}
+  for (let i=0; i>=character.length; i++) {
+    for (let j=0; j<=repeat; j++) {
+      after += character[i];
+    }
   }
-
+  display('repeated-output', after);
   // display the text with repeated characters
 });
